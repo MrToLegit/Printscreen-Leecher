13e17558ce30d15df408ac591fabecbf8d7f2709
MrToLegit <MrToLegit@users.noreply.github.com>
2019-10-31 17:17:19 +0100

    Update leecher.py

--- leecher.py
+++ leecher.py
@@ -5,6 +5,10 @@ from time import sleep
 import webbrowser
 from pathlib import Path
 import os
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 
 
 def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
@@ -21,6 +25,16 @@ def writeoutput():
 
 
 maxscanns = input("Enter how much scans do you want do run: ")
+openweb = input("Do you want to open automatically the links? [y/n]: ")
+
+while openweb != "y" and openweb != "n":
+    openweb = input("Do you want to open automatically the links? [y/n]: ") 
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
 
 for i in range(int(maxscanns), 0, -1):
     sleep(0.001)
@@ -29,8 +43,20 @@ for i in range(int(maxscanns), 0, -1):
         req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
         webpage = urlopen(req).read().decode("utf8")
 
+
+        driver.get(link)
+        try:
+            org = driver.find_element_by_id('screenshot-image')
+            # Find the value of org?
+            val = org.get_attribute("src")
+            if not ".png" in val:
+                continue
+        except:
+            print("Not Found: " + link)
+
         if '<div class="under-image">' in webpage:
-            webbrowser.open_new_tab(link)
-            print(link)
-            scanned.append(link)
+            if openweb == "y" or openweb == "Y":
+                webbrowser.open_new_tab(val)
+            print(val)
+            scanned.append(val)
             writeoutput()
