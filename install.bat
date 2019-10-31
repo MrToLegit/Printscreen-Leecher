38d6d55e7e964f4a86c15cb2071546c7fb1cb9cc
MrToLegit <MrToLegit@users.noreply.github.com>
2019-10-31 17:34:40 +0100

    Added a installation file

---
+++ install.bat
@@ -0,0 +1,9 @@
+@echo off
+pip install urllib
+pip install webbrowser
+pip install pathlib
+pip install os
+pip install selenium
+pip install webdriver_manager
+
+echo "You have installed everything! Run the Program with 'py leecher.py'"
